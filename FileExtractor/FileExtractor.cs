3f762cdf7c0ae54bb9dd184289a72d3c1b1d863f
marsonshine <ms27946@outlook.com>
2019-03-04 12:10:28 +0800

    fix:添加文件执行器和加载器

---
+++ FileExtractor/FileExtractor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileExtractor
+{
+    public class FileExtractor
+    {
+        private readonly FileFinder fileFinder;
+        private readonly FileLoader fileLoader;
+        public FileExtractor(FileFinder fileFinder,FileLoader fileLoader)
+        {
+            this.fileLoader = fileLoader;
+            this.fileFinder = fileFinder;
+        }
+
+
+    }
+}
---
+++ FileExtractor/FileLoader.cs
@@ -0,0 +1,7 @@
+﻿namespace FileExtractor
+{
+    public class FileLoader
+    {
+
+    }
+}
