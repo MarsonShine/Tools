a8ff166027c84d7259e55baa6c56abb5425bf63b
marsonshine <ms27946@outlook.com>
2020-06-09 21:58:04 +0800

    feat: 增加范型Handle 增加支持读取excel，单元格为表达式，以及单元格类型为single，double类型的值

--- ExcelCore/IExcelEventHandler.cs
+++ ExcelCore/IExcelEventHandler.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExcelCore
-{
-    public interface IExcelEventHandler
-    {
+namespace ExcelCore {
+    public interface IExcelEventHandler {
         Task Handle();
     }
+
+    public interface IExcelEventHandler<TExcelEntity>
+        where TExcelEntity : IExcelEntity {
+            Task Handle();
+        }
 }
--- ExcelCore/IExportExcelEventHandler.cs
+++ ExcelCore/IExportExcelEventHandler.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ExcelCore
-{
-    public interface IExportExcelEventHandler: IExcelEventHandler
-    {
+namespace ExcelCore {
+    public interface IExportExcelEventHandler : IExcelEventHandler {
         IList<IExcelEntity> Sources { get; }
         IList<ExcelOperationResultDescriptor> Results { get; }
     }
+
+    public interface IExportExcelEventHandler<TExcelEntity> : IExcelEventHandler<TExcelEntity>
+        where TExcelEntity : IExcelEntity {
+            IList<TExcelEntity> Sources { get; }
+            IList<ExcelOperationResultDescriptor> Results { get; }
+        }
 }
